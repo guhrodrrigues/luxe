302105bea66ac3b51fc5a958f34b41d499f2079e
guhrodriguess <gumartinsR1201@gmail.com>
2024-02-14 19:39:13 -0300

    style: Update texts component

--- src/components/ui/texts/TextAnimatedGradient.tsx
+++ src/components/ui/texts/TextAnimatedGradient.tsx
@@ -1,7 +1,7 @@
 export function TextAnimatedGradient() {
   return (
     <span className="inline-flex animate-text-gradient bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] text-3xl text-transparent font-medium bg-clip-text">
-      Text gradient
+      Text Animated Gradient
     </span>
   );
 }
