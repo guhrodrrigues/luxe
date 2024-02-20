e26d998635cd16a8e38b89b07ed10e186df9d9da
guhrodriguess <gumartinsR1201@gmail.com>
2024-02-19 21:29:05 -0300

    feat: Update footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/utils/cn";
-
-import author from "@/assets/author.jpg";
 
 export function Footer() {
   const pathname = usePathname();
@@ -13,16 +10,8 @@ export function Footer() {
   const isUiPage = pathname.startsWith("/ui");
 
   return (
-    <footer
-      className={cn(
-        "flex justify-center items-center gap-3 py-8",
-        isUiPage && "xl:pl-[250px]"
-      )}
-    >
-      <div>
-        <Image src={author} alt="Gustavo" width={30} className="rounded-full" />
-      </div>
-      <div>
+    <footer className={cn("py-10", isUiPage && "xl:pl-[250px]")}>
+      <div className="flex justify-center">
         <span className="text-sm">
           Made by{" "}
           <a
@@ -31,7 +20,7 @@ export function Footer() {
             rel="noopener noreferrer"
             className="text-secondary duration-300 hover:text-primary"
           >
-            Gustavo.
+            Gustavo Rodrigues.
           </a>
         </span>
       </div>
