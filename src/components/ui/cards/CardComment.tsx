4bfb9387ec2c93b8eba0fe3ae873f1718983c5a9
guhrodriguess <gumartinsR1201@gmail.com>
2024-02-19 21:29:16 -0300

    style: Update CardComment

--- src/components/ui/cards/CardComment.tsx
+++ src/components/ui/cards/CardComment.tsx
@@ -1,30 +1,30 @@
 import Image from "next/image";
 
+import author from "@/assets/author.jpg";
+
 export function CardComment() {
   return (
-    <div className="relative flex flex-col gap-6 bg-neutral-950 border border-white/10 w-full max-w-lg rounded-xl p-10">
+    <div className="relative flex flex-col gap-8 bg-neutral-950 border border-white/10 w-full max-w-lg rounded-xl p-10">
       <div className="absolute right-5 top-0 h-px w-80 bg-gradient-to-l from-transparent via-white/30 via-10% to-transparent" />
       <p className="text-base text-center leading-[1.5] text-neutral-300">
         “Luxe is an interesting library, with its ease of copying and pasting
         complex components without the need to install any lib and leaves us
         with the power to control our own components.”
       </p>
-      <div className="flex items-center justify-center gap-2">
+      <div className="flex items-center justify-center gap-3">
         <Image
-          src="https://github.com/guhrodriguess.png"
-          alt="Avatar"
-          width={32}
-          height={32}
+          src={author}
+          alt="Gustavo's profile image"
+          width={40}
+          height={40}
           className="rounded-full object-cover"
         />
-        <a
-          href="https://github.com/guhrodriguess"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-sm text-neutral-400 leading-none duration-300 hover:text-neutral-300"
-        >
-          Gustavo Rodrigues
-        </a>
+        <div className="flex flex-col">
+          <h1 className="text-base font-medium text-neutral-300 leading-none duration-300">
+            Gustavo Rodrigues
+          </h1>
+          <span className="text-neutral-400">Creator of Luxe</span>
+        </div>
       </div>
     </div>
   );
