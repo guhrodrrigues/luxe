db6aa6300c2a8eda98c1590e9e72a64822f24cea
guhrodriguess <gumartinsR1201@gmail.com>
2024-02-29 16:50:17 -0300

    fix: tidying up error

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,10 +1,11 @@
 import { HeroGrid } from "./components/HeroGrid";
 import { HeroContent } from "./components/HeroContent";
 
-export function HeroSection() {
+export async function HeroSection() {
   return (
     <section className="flex flex-col items-center justify-center relative min-h-[300px] [@media(min-height:793px)]:min-h-[500px]">
       <HeroGrid />
+      {/* @ts-expect-error Async Server Component */}
       <HeroContent />
     </section>
   );
