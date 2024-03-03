f679044a7f14865a1d66503cd4263535ebea87dd
guhrodriguess <gumartinsR1201@gmail.com>
2024-03-03 00:32:35 -0300

    feat(layout): update open-graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
       {
         width: 1920,
         height: 1080,
-        url: "https://i.imgur.com/5RUiyVn.png",
+        url: "https://i.imgur.com/1PB7OL8.png",
         alt: "Luxe website cover",
       },
     ],
@@ -70,7 +70,7 @@ export const metadata: Metadata = {
       {
         width: 1920,
         height: 1080,
-        url: "https://i.imgur.com/5RUiyVn.png",
+        url: "https://i.imgur.com/1PB7OL8.png",
         alt: "Luxe website cover",
       },
     ],
