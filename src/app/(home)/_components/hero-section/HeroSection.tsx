e4ec1f49d2407d8b66c054974feace52b87d5e7b
guhrodriguess <gumartinsR1201@gmail.com>
2024-03-06 22:36:42 -0300

    refactor: remove unused code

--- src/app/(home)/_components/hero-section/HeroSection.tsx
+++ src/app/(home)/_components/hero-section/HeroSection.tsx
@@ -5,21 +5,8 @@ export function HeroSection() {
   return (
     <section className="flex flex-col items-center justify-center relative min-h-[300px] [@media(min-height:793px)]:min-h-[600px]">
       <Spotlight />
-      <TopBlur />
       {/* @ts-expect-error Async Server Component */}
       <HeroContent />
     </section>
-  );
-}
-
-function TopBlur() {
-  return (
-    <div
-      className="pointer-events-none absolute left-0 top-0 z-[1] w-full backdrop-blur-[1px] will-change-transform h-[60px]"
-      style={{
-        background: "linear-gradient(to top,transparent,rgb(10,10,10))",
-        maskImage: "linear-gradient(to bottom,rgb(10,10,10) 25%,transparent)",
-      }}
-    />
   );
 }
