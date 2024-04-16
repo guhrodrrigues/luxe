6ad5e2084415d19c4b1b953d361fc1731f38f61f
guhrodriguess <gumartinsr1201@gmail.com>
2024-04-16 17:04:54 -0300

    style(component-view): update height

--- src/app/(ui)/ui/_components/component-page/ComponentView.tsx
+++ src/app/(ui)/ui/_components/component-page/ComponentView.tsx
@@ -6,7 +6,7 @@ export function ComponentView({ className, children }: ComponentViewProps) {
   return (
     <div
       className={cn(
-        "p-8 min-h-[200px] flex items-center justify-center overflow-hidden bg-neutral-950 shadow border border-neutral-900 rounded-xl",
+        "p-8 min-h-[250px] flex items-center justify-center overflow-hidden bg-neutral-950 shadow border border-neutral-900 rounded-xl",
         className
       )}
     >
