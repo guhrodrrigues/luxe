640e5f8debf1a62e0b13d0c9722dce7ccada67ae
guhrodrrigues <gumartinsr1201@gmail.com>
2024-11-08 22:58:20 -0300

    style: update animated tabs

--- src/app/_components/ui/tabs/AnimatedTabs.tsx
+++ src/app/_components/ui/tabs/AnimatedTabs.tsx
@@ -4,22 +4,22 @@
 import { useEffect, useRef, useState } from "react";
 
 const TABS = [
-  { name: "All Posts" },
-  { name: "Interactions" },
-  { name: "Resources" },
-  { name: "Docs" },
+  { label: "All Posts" },
+  { label: "Interactions" },
+  { label: "Resources" },
+  { label: "Docs" },
 ];
 
 export function AnimatedTabs() {
-  const [activeTab, setActiveTab] = useState(TABS[0].name);
-  const containerRef = useRef<any>(null);
-  const activeTabElementRef = useRef<any>(null);
+  const [activeTab, setActiveTab] = useState(TABS[0].label);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const activeTabRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const container = containerRef.current;
 
-    if (activeTab && container) {
-      const activeTabElement = activeTabElementRef.current;
+    if (container && activeTab) {
+      const activeTabElement = activeTabRef.current;
 
       if (activeTabElement) {
         const { offsetLeft, offsetWidth } = activeTabElement;
@@ -30,47 +30,42 @@ export function AnimatedTabs() {
         container.style.clipPath = `inset(0 ${Number(100 - (clipRight / container.offsetWidth) * 100).toFixed()}% 0 ${Number((clipLeft / container.offsetWidth) * 100).toFixed()}% round 17px)`;
       }
     }
-  }, [activeTab, activeTabElementRef, containerRef]);
+  }, [activeTab, activeTabRef, containerRef]);
 
   return (
     <div className="relative mx-auto flex w-fit flex-col items-center rounded-full">
-      <ul className="relative flex w-full justify-center">
-        {TABS.map((tab) => (
-          <li key={tab.name}>
-            <button
-              ref={activeTab === tab.name ? activeTabElementRef : null}
-              data-tab={tab.name}
-              onClick={() => {
-                setActiveTab(tab.name);
-              }}
-              className="flex h-8 items-center rounded-full p-3 text-sm font-medium text-neutral-200"
-            >
-              {tab.name}
-            </button>
-          </li>
-        ))}
-      </ul>
       <div
-        aria-hidden
-        className="absolute z-10 w-full overflow-hidden [clip-path:inset(0px_75%_0px_0%_round_17px)] [transition:clip-path_0.25s_ease]"
         ref={containerRef}
+        className="absolute z-10 w-full overflow-hidden [clip-path:inset(0px_75%_0px_0%_round_17px)] [transition:clip-path_0.25s_ease]"
       >
-        <ul className="relative flex w-full justify-center bg-white">
-          {TABS.map((tab) => (
-            <li key={tab.name}>
-              <button
-                data-tab={tab.name}
-                onClick={() => {
-                  setActiveTab(tab.name);
-                }}
-                className="flex h-8 items-center rounded-full p-3 text-sm font-medium text-black"
-                tabIndex={-1}
-              >
-                {tab.name}
-              </button>
-            </li>
+        <div className="relative flex w-full justify-center bg-white">
+          {TABS.map((tab, index) => (
+            <button
+              key={index}
+              onClick={() => setActiveTab(tab.label)}
+              className="flex h-8 items-center rounded-full p-3 text-sm font-medium text-black"
+              tabIndex={-1}
+            >
+              {tab.label}
+            </button>
           ))}
-        </ul>
+        </div>
+      </div>
+      <div className="relative flex w-full justify-center">
+        {TABS.map((tab, index) => {
+          const isActive = activeTab === tab.label;
+
+          return (
+            <button
+              key={index}
+              ref={isActive ? activeTabRef : null}
+              onClick={() => setActiveTab(tab.label)}
+              className="flex h-8 items-center rounded-full p-3 text-sm font-medium text-neutral-300"
+            >
+              {tab.label}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
