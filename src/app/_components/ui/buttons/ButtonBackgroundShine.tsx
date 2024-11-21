d7faa6342abcdd07abe5532897e33a75abdb9ef2
guhrodrrigues <gumartinsr1201@gmail.com>
2024-11-20 22:55:00 -0300

    style: update shine components

--- src/app/_components/ui/buttons/ButtonBackgroundShine.tsx
+++ src/app/_components/ui/buttons/ButtonBackgroundShine.tsx
@@ -1,6 +1,6 @@
 export function ButtonBackgroundShine() {
   return (
-    <button className="inline-flex animate-shine items-center justify-center rounded-xl border border-neutral-800 bg-[linear-gradient(110deg,#000103,45%,#303030,55%,#000103)] bg-[length:200%_100%] px-4 py-2 text-sm font-medium text-neutral-400 transition-colors">
+    <button className="animate-shine items-center justify-center rounded-xl border border-neutral-800 bg-[linear-gradient(110deg,#000103,45%,#303030,55%,#000103)] bg-[length:400%_100%] px-4 py-2 text-sm font-medium text-neutral-400 transition-colors">
       Button
     </button>
   );
--- src/app/_components/ui/texts/TextShine.tsx
+++ src/app/_components/ui/texts/TextShine.tsx
@@ -1,6 +1,6 @@
 export function TextShine() {
   return (
-    <h1 className="animate-shine bg-[linear-gradient(110deg,#737373,45%,#fff,55%,#737373)] bg-[length:400%_100%] bg-clip-text text-base font-medium text-transparent">
+    <h1 className="animate-shine bg-[linear-gradient(110deg,#404040,45%,#fff,55%,#404040)] bg-[length:400%_100%] bg-clip-text text-base font-medium text-transparent">
       Generating code...
     </h1>
   );
