6c5cb9c25ea02b4cf6d0df935e8bb7e994eb484d
Lucas Augusto <lucasaugustscode@gmail.com>
2025-04-25 20:49:49 -0300

    refactor(www): add line-height

--- apps/www/src/app/(home)/_components/hero-section/HeroContent.tsx
+++ apps/www/src/app/(home)/_components/hero-section/HeroContent.tsx
@@ -1,42 +1,53 @@
-import { ButtonGlitchBrightness } from "@/app/_components/ButtonGlitchBrightness";
-import { Icons } from "@/app/_components/Icons";
-import { TextAnimateEnter } from "@/app/_components/TextAnimateEnter";
-import { AnimateEnter } from "../AnimateEnter";
-import { GridBackground } from "../GridBackground";
-import { AnimatedNumber } from "./AnimatedNumber";
-import { Techs } from "../techs-section/Techs";
+import { ButtonGlitchBrightness } from '@/app/_components/ButtonGlitchBrightness'
+import { Icons } from '@/app/_components/Icons'
+import { TextAnimateEnter } from '@/app/_components/TextAnimateEnter'
+import { AnimateEnter } from '../AnimateEnter'
+import { GridBackground } from '../GridBackground'
+import { AnimatedNumber } from './AnimatedNumber'
+import { Techs } from '../techs-section/Techs'
+
+import { cn } from '@/utils/cn'
 
 async function getRepoStarCount() {
-  const res = await fetch("https://api.github.com/repos/guhrodrrigues/luxe");
-  const data = await res.json();
-  const starCount = data.stargazers_count;
+  const res = await fetch('https://api.github.com/repos/guhrodrrigues/luxe')
+  const data = await res.json()
+  const starCount = data.stargazers_count
 
   if (starCount > 999) {
-    return (starCount / 1000).toFixed(1) + "K";
+    return (starCount / 1000).toFixed(1) + 'K'
   }
 
-  return starCount;
+  return starCount
 }
 
 export async function HeroContent() {
-  const starCount = await getRepoStarCount();
+  const starCount = await getRepoStarCount()
 
   return (
-    <div className="z-[3] flex flex-col items-center gap-20 text-center">
-      <div className="space-y-10">
+    <div className={cn('z-[3] flex flex-col items-center gap-20 text-center')}>
+      <div className={cn('space-y-10')}>
         <AnimateEnter delay={0.1}>
-          <h1 className="text-gradient mx-auto max-w-3xl text-4xl font-semibold leading-[calc(72px*1.4] tracking-tight md:text-7xl">
+          <h1
+            className={cn(
+              'mx-auto max-w-3xl font-semibold text-4xl text-gradient leading-[calc(4.5rem_*_1.2)] tracking-tight',
+              'md:text-7xl',
+            )}
+          >
             Illuminate your apps. Fand and Easy.
           </h1>
         </AnimateEnter>
         <AnimateEnter delay={0.2}>
-          <p className="mx-auto text-xl max-w-lg text-neutral-600 dark:text-neutral-300">
+          <p
+            className={cn(
+              'mx-auto max-w-lg text-neutral-600 text-xl dark:text-neutral-300',
+            )}
+          >
             Library of components copy and paste to illuminate your applications
             with elegance and sophistication.
           </p>
         </AnimateEnter>
         <AnimateEnter
-          className="flex flex-wrap items-center justify-center gap-2"
+          className={cn('flex flex-wrap items-center justify-center gap-2')}
           delay={0.3}
         >
           <ButtonGlitchBrightness href="/ui/installation" text="Get Started" />
@@ -44,7 +55,9 @@ export async function HeroContent() {
             href="https://github.com/guhrodrrigues/luxe"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-1.5 rounded-xl border border-border bg-background px-4 py-2 text-sm font-semibold text-neutral-600 duration-300 hover:bg-neutral-200 dark:text-secondary dark:hover:bg-neutral-900"
+            className={cn(
+              'flex items-center gap-1.5 rounded-xl border border-border bg-background px-4 py-2 text-sm font-semibold text-neutral-600 duration-300 hover:bg-neutral-200 dark:text-secondary dark:hover:bg-neutral-900',
+            )}
           >
             <Icons.github className="size-3.5" />
             <AnimatedNumber value={starCount} />
@@ -57,5 +70,5 @@ export async function HeroContent() {
       </AnimateEnter>
       <GridBackground />
     </div>
-  );
+  )
 }
--- apps/www/src/app/_components/ui/badge-animated-border.tsx
+++ apps/www/src/app/_components/ui/badge-animated-border.tsx
@@ -1,22 +1,33 @@
-import { cn } from "@/utils/cn";
+import { cn } from '@/utils/cn'
 
-export function BadgeAnimatedBorder() {
+type BadgeAnimatedBorderProps = React.ComponentPropsWithRef<'div'>
+
+export function BadgeAnimatedBorder({
+  children,
+  className,
+  ...props
+}: BadgeAnimatedBorderProps) {
   return (
     <div className="group relative grid overflow-hidden rounded-full px-3 py-1 shadow-[0_1000px_0_0_hsl(0_0%_85%)_inset] transition-colors duration-200 dark:shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset]">
       <span>
         <span
           className={cn(
-            "spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full",
-            "[mask:linear-gradient(black,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] dark:[mask:linear-gradient(white,_transparent_50%)]",
-            "before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,black_360deg)] dark:before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)]",
+            'spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full',
+            '[mask:linear-gradient(black,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] dark:[mask:linear-gradient(white,_transparent_50%)]',
+            'before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,black_360deg)] dark:before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)]',
             "before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]",
           )}
         />
       </span>
       <span className="backdrop absolute inset-px rounded-full bg-neutral-100 transition-colors duration-200 dark:bg-neutral-950" />
-      <span className="z-10 text-xs font-medium text-neutral-500 dark:text-neutral-400">
-        Badge
+      <span
+        className={cn(
+          'z-10 text-xs font-medium text-neutral-500 dark:text-neutral-400',
+          className,
+        )}
+      >
+        {children}
       </span>
     </div>
-  );
+  )
 }
