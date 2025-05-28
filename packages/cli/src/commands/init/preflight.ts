ba45196aec1aed56adb82c41af3c0b972c4b4ae5
Lucas Augusto <lucasaugustscode@gmail.com>
2025-05-27 22:06:49 -0300

    fix(cli): fixing regex version

--- packages/cli/src/commands/init/preflight.ts
+++ packages/cli/src/commands/init/preflight.ts
@@ -28,7 +28,7 @@ export async function preFlight() {
 
   const tailwindDependency = 'tailwindcss'
   const regexValidateTailwindVersion =
-    /^\s*(?:\^|~|>=|<=|>|<|=)?\s*4\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?\s*$/ // 4.x.x
+    /^\s*(?:\^|~|>=|<=|>|<|=)?\s*4\.\d+(?:\.\d+)?(?:-[0-9A-Za-z.-]+)?\s*$/ // 4.x or 4.x.x
 
   if (!(tailwindDependency in dependencies)) {
     errorsFound[InitCommandErrors.TAILWIND_NOT_INSTALLED] = true
