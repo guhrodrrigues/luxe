4a6134b32cd2fde4f9634775afb20f5c4b4f739f
guhrodrrigues <gumartinsr1201@gmail.com>
2025-05-29 23:14:33 -0300

    feat: update components

--- apps/www/src/app/_components/ui/text.tsx
+++ apps/www/src/app/_components/ui/text.tsx
@@ -19,6 +19,7 @@ const variants = [
         className={cn(
           'bg-[linear-gradient(110deg,#bfbfbf,35%,#000,50%,#bfbfbf,75%,#bfbfbf)] dark:bg-[linear-gradient(110deg,#404040,35%,#fff,50%,#404040,75%,#404040)]',
           'bg-[length:200%_100%] bg-clip-text text-transparent',
+					className
         )}
         initial={{ backgroundPosition: '200% 0' }}
         animate={{ backgroundPosition: '-200% 0' }}
