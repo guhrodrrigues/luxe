ce75e160b97173746d5582d0fbd246125c4fc7d6
guhrodrrigues <gumartinsr1201@gmail.com>
2025-06-09 14:25:28 -0300

    feat(apps/www): update navigation menu docs

--- packages/react/__stories__/components/navigation-menu.stories.tsx
+++ packages/react/__stories__/components/navigation-menu.stories.tsx
@@ -1,68 +1,68 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react'
 
 import {
   NavigationMenu,
   NavigationMenuContent,
+  NavigationMenuContentItem,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuListItem,
   NavigationMenuTrigger,
-} from "@/components/navigation-menu";
+} from '@/components/navigation-menu'
 
 const OVERVIEW = [
   {
-    title: "Quick Start",
-    description: "Install and assemble your first component.",
+    title: 'Quick Start',
+    description: 'Install and assemble your first component.',
   },
   {
-    title: "About",
-    description: "Learn more about Luxe and our mission.",
+    title: 'About',
+    description: 'Learn more about Luxe and our mission.',
   },
   {
-    title: "Accessibility",
-    description: "Learn how we build accessible components.",
+    title: 'Accessibility',
+    description: 'Learn how we build accessible components.',
   },
   {
-    title: "Releases",
-    description: "See what’s new in the latest Luxe versions.",
+    title: 'Releases',
+    description: 'See what’s new in the latest Luxe versions.',
   },
-];
+]
 
 const DOCS = [
   {
-    title: "Accordion",
-    description: "A vertically stacked set of interactive headings.",
+    title: 'Accordion',
+    description: 'A vertically stacked set of interactive headings.',
   },
   {
-    title: "Checkbox",
-    description: "A control that allows users to select one or more options.",
+    title: 'Checkbox',
+    description: 'A control that allows users to select one or more options.',
   },
   {
-    title: "Dialog",
-    description: "A modal window that appears on top of the main content.",
+    title: 'Dialog',
+    description: 'A modal window that appears on top of the main content.',
   },
   {
-    title: "Tooltip",
-    description: "A small popup that appears when hovering over an element.",
+    title: 'Tooltip',
+    description: 'A small popup that appears when hovering over an element.',
   },
   {
-    title: "Multi Step Modal",
+    title: 'Multi Step Modal',
     description:
-      "A modal dialog that guides users through a sequence of steps.",
+      'A modal dialog that guides users through a sequence of steps.',
   },
   {
-    title: "Switch",
+    title: 'Switch',
     description:
-      "A toggle control that allows users to turn an option on or off.",
+      'A toggle control that allows users to turn an option on or off.',
   },
-];
+]
 
 const meta: Meta = {
-  title: "components/NavigationMenu",
+  title: 'components/NavigationMenu',
   component: NavigationMenu,
   parameters: {
-    layout: "centered",
+    layout: 'centered',
   },
   args: {
     children: (
@@ -72,13 +72,13 @@ const meta: Meta = {
             <NavigationMenuTrigger>Overview</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid w-[400px] md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                {OVERVIEW.map((component) => (
-                  <NavigationMenuListItem key={component.title} href="#">
+                {OVERVIEW.map(component => (
+                  <NavigationMenuContentItem key={component.title} href="#">
                     <h3 className="text-primary">{component.title}</h3>
                     <span className="text-primary-muted">
                       {component.description}
                     </span>
-                  </NavigationMenuListItem>
+                  </NavigationMenuContentItem>
                 ))}
               </ul>
             </NavigationMenuContent>
@@ -87,13 +87,13 @@ const meta: Meta = {
             <NavigationMenuTrigger>Docs</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid w-[400px] md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                {DOCS.map((component) => (
-                  <NavigationMenuListItem key={component.title} href="#">
+                {DOCS.map(component => (
+                  <NavigationMenuContentItem key={component.title} href="#">
                     <h3 className="text-primary">{component.title}</h3>
                     <span className="text-primary-muted">
                       {component.description}
                     </span>
-                  </NavigationMenuListItem>
+                  </NavigationMenuContentItem>
                 ))}
               </ul>
             </NavigationMenuContent>
@@ -105,8 +105,8 @@ const meta: Meta = {
       </NavigationMenu>
     ),
   },
-};
+}
 
-export default meta;
+export default meta
 
-export const Basic: StoryObj = {};
+export const Basic: StoryObj = {}
--- packages/react/src/components/navigation-menu.tsx
+++ packages/react/src/components/navigation-menu.tsx
@@ -1,15 +1,13 @@
-"use client"; // @NOTE: Add in case you are using Next.js
+'use client' // @NOTE: Add in case you are using Next.js
 
-import { useState } from "react";
+import { useState } from 'react'
 
-import * as RadixNavigationMenu from "@radix-ui/react-navigation-menu";
-import { AnimatePresence, motion, useReducedMotion } from "motion/react";
+import * as RadixNavigationMenu from '@radix-ui/react-navigation-menu'
+import { AnimatePresence, motion, useReducedMotion } from 'motion/react'
 
-import { cn } from "@/utils/cn";
+import { cn } from '@/utils/cn'
 
-type NavigationMenuProps = React.ComponentProps<
-  typeof RadixNavigationMenu.Root
->;
+type NavigationMenuProps = React.ComponentProps<typeof RadixNavigationMenu.Root>
 
 export function NavigationMenu({
   className,
@@ -21,19 +19,19 @@ export function NavigationMenu({
       data-slot="navigation-menu"
       data-viewport={false}
       className={cn(
-        "relative flex max-w-max flex-1 items-center justify-center",
+        'relative flex max-w-max flex-1 items-center justify-center',
         className,
       )}
       {...props}
     >
       {children}
     </RadixNavigationMenu.Root>
-  );
+  )
 }
 
 type NavigationMenuListProps = React.ComponentProps<
   typeof RadixNavigationMenu.List
->;
+>
 
 export function NavigationMenuList({
   className,
@@ -43,17 +41,17 @@ export function NavigationMenuList({
     <RadixNavigationMenu.List
       data-slot="navigation-menu-list"
       className={cn(
-        "group flex flex-1 list-none items-center justify-center gap-1",
+        'group flex flex-1 list-none items-center justify-center gap-1',
         className,
       )}
       {...props}
     />
-  );
+  )
 }
 
 type NavigationMenuItemProps = React.ComponentProps<
   typeof RadixNavigationMenu.Item
->;
+>
 
 export function NavigationMenuItem({
   className,
@@ -62,15 +60,15 @@ export function NavigationMenuItem({
   return (
     <RadixNavigationMenu.Item
       data-slot="navigation-menu-item"
-      className={cn("relative", className)}
+      className={cn('relative', className)}
       {...props}
     />
-  );
+  )
 }
 
 type NavigationMenuTriggerProps = React.ComponentProps<
   typeof RadixNavigationMenu.Trigger
->;
+>
 
 export function NavigationMenuTrigger({
   className,
@@ -81,9 +79,9 @@ export function NavigationMenuTrigger({
     <RadixNavigationMenu.Trigger
       data-slot="navigation-menu-trigger"
       className={cn(
-        "group inline-flex h-9 w-max items-center justify-center rounded-md bg-main-background px-4 py-2 font-medium text-sm hover:bg-main-foreground hover:text-primary",
-        "text-primary-muted focus:bg-main-foreground focus:text-primary disabled:pointer-events-none disabled:opacity-50 data-[state=open]:text-primary",
-        "outline-none data-[state=open]:bg-main-foreground data-[state=open]:focus:bg-main-foreground",
+        'group inline-flex h-9 w-max items-center justify-center rounded-md bg-main-background px-4 py-2 font-medium text-sm hover:bg-main-foreground hover:text-primary',
+        'text-primary-muted focus:bg-main-foreground focus:text-primary disabled:pointer-events-none disabled:opacity-50 data-[state=open]:text-primary',
+        'outline-none data-[state=open]:bg-main-foreground data-[state=open]:focus:bg-main-foreground',
         className,
       )}
       {...props}
@@ -92,7 +90,7 @@ export function NavigationMenuTrigger({
       <svg
         aria-hidden
         xmlns="http://www.w3.org/2000/svg"
-        className="relative top-[1px] ml-1 size-3 transition duration-200 group-data-[state=open]:rotate-180"
+        className="relative top-[1px] ml-1 size-3 group-data-[state=open]:rotate-180 motion-safe:transition motion-safe:duration-200"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
@@ -104,12 +102,12 @@ export function NavigationMenuTrigger({
         <path d="m6 9 6 6 6-6" />
       </svg>
     </RadixNavigationMenu.Trigger>
-  );
+  )
 }
 
 type NavigationMenuContentProps = React.ComponentProps<
   typeof RadixNavigationMenu.Content
->;
+>
 
 export function NavigationMenuContent({
   className,
@@ -119,67 +117,51 @@ export function NavigationMenuContent({
     <RadixNavigationMenu.Content
       data-slot="navigation-menu-content"
       className={cn(
-        "top-full mt-1.5 overflow-hidden rounded-xl border bg-main-muted z-50 text-primary-muted shadow left-0 w-full border-border p-2.5 md:absolute md:left-1/2 md:w-auto",
-        "**:data-[slot=navigation-menu-link]:focus:outline-none **:data-[slot=navigation-menu-link]:focus:ring-0",
-        "motion-safe:data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52",
-        "motion-safe:data-[motion=to-end]:slide-out-to-right-52 motion-safe:data-[motion=to-start]:slide-out-to-left-52 md:-translate-x-1/2",
-        "motion-safe:data-[state=closed]:zoom-out-95 motion-safe:data-[state=open]:zoom-in-95 motion-safe:data-[motion^=from-]:animate-in motion-safe:data-[motion^=to-]:animate-out",
-        "motion-safe:duration-200 motion-safe:data-[state=closed]:animate-out motion-safe:data-[state=open]:animate-in",
-        "motion-safe:data-[state=open]:fade-in-0 motion-safe:data-[state=closed]:zoom-out-95 motion-safe:data-[state=open]:zoom-in-95 motion-safe:data-[state=closed]:fade-out-0",
+        'top-full left-0 z-50 mt-1.5 w-full overflow-hidden rounded-xl border border-border bg-main-muted p-2.5 text-primary-muted shadow md:absolute md:left-1/2 md:w-auto',
+        '**:data-[slot=navigation-menu-link]:focus:outline-none **:data-[slot=navigation-menu-link]:focus:ring-0',
+        'motion-safe:data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52',
+        'motion-safe:data-[motion=to-end]:slide-out-to-right-52 motion-safe:data-[motion=to-start]:slide-out-to-left-52 md:-translate-x-1/2',
+        'motion-safe:data-[state=closed]:zoom-out-95 motion-safe:data-[state=open]:zoom-in-95 motion-safe:data-[motion^=from-]:animate-in motion-safe:data-[motion^=to-]:animate-out',
+        'motion-safe:duration-200 motion-safe:data-[state=closed]:animate-out motion-safe:data-[state=open]:animate-in',
+        'motion-safe:data-[state=open]:fade-in-0 motion-safe:data-[state=closed]:zoom-out-95 motion-safe:data-[state=open]:zoom-in-95 motion-safe:data-[state=closed]:fade-out-0',
         className,
       )}
       {...props}
     />
-  );
+  )
 }
 
-type NavigationMenuLinkProps = React.ComponentProps<
+type NavigationMenuContentItemProps = React.ComponentProps<
   typeof RadixNavigationMenu.Link
->;
+>
 
-export function NavigationMenuLink({
-  className,
-  ...props
-}: NavigationMenuLinkProps) {
-  return (
-    <RadixNavigationMenu.Link
-      data-slot="navigation-menu-link"
-      className={cn(
-        "text-primary-muted data-[active=true]:bg-main-foreground data-[active=true]:text-primary data-[active=true]:focus:bg-main-foreground",
-        "flex hover:bg-main-foreground hover:text-primary focus:bg-main-foreground focus:text-primary data-[active=true]:hover:bg-main-foreground",
-        "h-9 flex-col gap-1 rounded-md px-4 py-2 font-medium text-sm outline-none transition-all focus-within:ring-border focus-visible:outline-1",
-        'focus-visible:ring-1 [&_svg:not([class*="size-"])]:size-4 [&_svg:not([class*="text-"])]:text-main-muted',
-        className,
-      )}
-      {...props}
-    />
-  );
-}
-
-type NavigationMenuListItemProps = React.ComponentProps<
-  typeof RadixNavigationMenu.Link
->;
-
-export function NavigationMenuListItem({
+export function NavigationMenuContentItem({
   children,
   className,
   ...props
-}: NavigationMenuListItemProps) {
-  const [isHovered, setIsHovered] = useState(false);
+}: NavigationMenuContentItemProps) {
+  const [isHovered, setIsHovered] = useState(false)
 
-  const isReducedMotion = useReducedMotion();
+  const isReducedMotion = useReducedMotion()
+
+  const interactionProps = !isReducedMotion && {
+    onMouseEnter: () => setIsHovered(true),
+    onMouseLeave: () => setIsHovered(false),
+    onFocus: () => setIsHovered(true),
+    onBlur: () => setIsHovered(false),
+  }
 
   return (
     <RadixNavigationMenu.Link
+      data-slot="navigation-menu-content-item"
+      className={cn(
+        'relative rounded-lg p-3 outline-none',
+        isReducedMotion && 'hover:bg-main-foreground',
+      )}
       {...props}
-      data-slot="navigation-menu-list-item"
-      className="relative rounded-lg p-3 outline-none"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onFocus={() => setIsHovered(true)}
-      onBlur={() => setIsHovered(false)}
+      {...interactionProps}
     >
-      <div className={cn("relative z-10 flex flex-col gap-0.5", className)}>
+      <div className={cn('relative z-10 flex flex-col gap-0.5', className)}>
         {children}
       </div>
       {!isReducedMotion && (
@@ -193,7 +175,7 @@ export function NavigationMenuListItem({
               exit={{ opacity: 0 }}
               transition={{
                 duration: 0.3,
-                type: "spring",
+                type: 'spring',
                 bounce: 0,
               }}
             />
@@ -201,5 +183,28 @@ export function NavigationMenuListItem({
         </AnimatePresence>
       )}
     </RadixNavigationMenu.Link>
-  );
+  )
+}
+
+type NavigationMenuLinkProps = React.ComponentProps<
+  typeof RadixNavigationMenu.Link
+>
+
+export function NavigationMenuLink({
+  className,
+  ...props
+}: NavigationMenuLinkProps) {
+  return (
+    <RadixNavigationMenu.Link
+      data-slot="navigation-menu-link"
+      className={cn(
+        'text-primary-muted data-[active=true]:bg-main-foreground data-[active=true]:text-primary data-[active=true]:focus:bg-main-foreground',
+        'flex hover:bg-main-foreground hover:text-primary focus:bg-main-foreground focus:text-primary data-[active=true]:hover:bg-main-foreground',
+        'h-9 flex-col gap-1 rounded-md px-4 py-2 font-medium text-sm outline-none transition-all focus-within:ring-border focus-visible:outline-1',
+        'focus-visible:ring-1 [&_svg:not([class*="size-"])]:size-4 [&_svg:not([class*="text-"])]:text-main-muted',
+        className,
+      )}
+      {...props}
+    />
+  )
 }
